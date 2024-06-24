c922d193289ec96fe3393ff7e109ff9b91e428cd
Youssef <youssefkaramedu@gmail.com>
2024-06-24 16:51:38 +0000

    Add file 0

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -2,8 +2,8 @@
 
 ''' module 1 '''
 from typing import Tuple
- 
- 
+
+
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     ''' function to calc index '''
     start = (page - 1) * page_size
