817cb31e2ff2d97b90916ad6629872ddcf410d9b
Youssef <youssefkaramedu@gmail.com>
2024-06-24 17:09:58 +0000

    Add file 1

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -34,11 +34,17 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Retrieves a page of data.
 
-        assert type(page) == int and type(page_size) == int
-        assert page > 0 and page_size > 0
-        start, end = index_range(page, page_size)
-        data = self.dataset()
-        if start > len(data):
-            return []
-        return data[start:end]
+        """
+    assert isinstance(page, int) and page > 0, "Page positive "
+    assert isinstance(page_size, int) and page_size > 0, "Page size positive"
+
+    start, end = index_range(page, page_size)
+    data = self.dataset()
+
+    if start >= len(data):
+        return []
+
+    return data[start:end]
