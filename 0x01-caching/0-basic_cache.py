03bcaec23a4193eb53cedf0abafbd920d622c1fd
Youssef <youssefkaramedu@gmail.com>
2024-06-26 17:11:29 +0000

    Add doc to class methods

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -7,11 +7,13 @@ from base_caching import BaseCaching
 class BasicCache(BaseCaching):
     ''' basicchache that inherits '''
     def put(self, key, item):
+        ''' put func '''
         if key is None or item is None:
             return
         self.cache_data[key] = item
 
     def get(self, key):
+        ''' get function '''
         if key is None or item is None:
             return None
         return self.cache_data[key]
