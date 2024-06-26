bd979e085782f88bae4c920726e9d53e2df64f74
Youssef <youssefkaramedu@gmail.com>
2024-06-26 17:39:54 +0000

    Add file 2 with correct class name

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -5,7 +5,7 @@ from base_caching import BaseCaching
 from collections import OrderedDict
 
 
-class FIFOCache(BaseCaching):
+class LIFOCache(BaseCaching):
     ''' FIFOCache  inherits from BaseCaching and implements FIFO caching '''
 
     def __init__(self):
