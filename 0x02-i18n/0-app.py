7fb4a22cc012d85a2c7578f061a6e8d6f6b292a3
Youssef <youssefkaramedu@gmail.com>
2024-07-03 00:24:08 +0000

    Add file 0

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" module """
+
+from flask import Flask, render_template
+
+
+pp = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('0-index.html')
