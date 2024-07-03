a53656938ea1ece693be7c1a2b8b7bf67ff9989e
Youssef Karam <92326279+callmejoee@users.noreply.github.com>
2024-07-03 03:32:40 +0300

    Update 0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -11,3 +11,7 @@ pp = Flask(__name__)
 def home() -> str:
     ''' function to render index '''
     return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
