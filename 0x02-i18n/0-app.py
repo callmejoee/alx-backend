d4b921a68c3392a6aef93b9513c2191450914279
Youssef Karam <92326279+callmejoee@users.noreply.github.com>
2024-07-03 03:33:16 +0300

    Update 0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -3,7 +3,6 @@
 
 from flask import Flask, render_template
 
-app.url_map.strict_slashes = False
 pp = Flask(__name__)
 
 
@@ -11,7 +10,3 @@ pp = Flask(__name__)
 def home() -> str:
     ''' function to render index '''
     return render_template('0-index.html')
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
